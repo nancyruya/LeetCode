34103862f63521af6fefd3a8401bfe6aea12ea61
nancyruya <34261425+nancyruya@users.noreply.github.com>
2019-11-18 18:15:28 -0500

    Create 83RemoveDuplicatesFromSortedList.py

---
+++ 83RemoveDuplicatesFromSortedList.py
@@ -0,0 +1,8 @@
+class Solution:
+    def deleteDuplicates(self, head: ListNode) -> ListNode:
+        cur = head
+        while cur:
+            while cur.next and cur.next.val == cur.val:
+                cur.next = cur.next.next     # skip duplicated node
+            cur = cur.next     # not duplicate of current node, move to next node
+        return head
