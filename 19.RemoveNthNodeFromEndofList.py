842d047476d7339d217f242f5bf10c00e6ca75aa
nancyruya <34261425+nancyruya@users.noreply.github.com>
2020-02-03 20:30:13 -0500

    Create 19.RemoveNthNodeFromEndofList.py

---
+++ 19.RemoveNthNodeFromEndofList.py
@@ -0,0 +1,12 @@
+class Solution:
+    def removeNthFromEnd(self, head: ListNode, n: int) -> ListNode:
+        dummy = ListNode(0)
+        dummy.next = head
+        fast = slow = dummy
+        for _ in range(n):
+            fast = fast.next
+        while fast and fast.next:
+            fast = fast.next
+            slow = slow.next
+        slow.next = slow.next.next
+        return dummy.next
