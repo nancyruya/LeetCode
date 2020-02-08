cf34bf71a554c4227711a18ac9bc19408c9853bb
nancyruya <34261425+nancyruya@users.noreply.github.com>
2020-02-07 20:18:08 -0500

    Create 34.FindFirstandLastPositionofElementinSortedArray.py

---
+++ 34.FindFirstandLastPositionofElementinSortedArray.py
@@ -0,0 +1,13 @@
+class Solution:
+    def searchRange(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            if nums[i] == target:
+                left_idx = i
+                break
+        else:
+            return [-1, -1]
+        for j in range(len(nums)-1, -1, -1):
+            if nums[j] == target:
+                right_idx = j
+                break
+        return [left_idx, right_idx]
